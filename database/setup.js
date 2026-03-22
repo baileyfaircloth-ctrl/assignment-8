1ea7f0b0c67fcf68f352c813b45d89a541f6ad86
baileyfaircloth-ctrl <baileyfaircloth@abbey.bac.edu>
2026-03-22 08:49:46 -0400

    Update setup.js

--- database/setup.js
+++ database/setup.js
@@ -88,3 +88,24 @@ async function setupDatabase() {
 if (require.main === module) {
     setupDatabase();
 }
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
